60f092eb689747841a659c17aaad32877ca6c1d2
Michael Godfrey <m.godfrey59@gmail.com>
2023-05-05 20:42:50 +1200

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ethan's Coffee Heaven
+
+## Acknowledgements
+
+Mobile menu drawer JavaScript and CSS from [this repository](https://github.com/tomaszbujnowicz/vanilla-js-drawer).
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,15 @@ module.exports = {
         misc: "#999999",
         background: "#f6f6f2",
         primary: "#ffd166",
+        primaryHover: "#ffc233",
         secondary: "#6d4c41",
       },
       backgroundImage: {
-        "banner": "url('/images/banner.webp')",
-      }
+        banner: "url('/images/banner.webp')",
+        beans: "url('/images/bg-coffee-beans.webp')",
+        "beans-cover": "url('/images/coffee-beans-cover.webp')",
+        "beans-wall": "url('/images/coffee-beans-wall.webp')",
+      },
     },
   },
   plugins: [],
